10138a3143721dc5eaf152ab0af82f4b1bf186dd
diegofrancke <diegofrancke@gmail.com>
2024-07-01 22:41:20 -0300

    Atividade 1 - Ajustes

---
+++ Atividade 1/atividade1-juan/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World!
+        </p>
+        <p>
+          Atividade 1 (Programação Orientada a Componentes)
+        </p>
+        <p>
+          Juan Diego Francke Cardoso
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
